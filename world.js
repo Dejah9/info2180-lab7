0859660f238aa502e5e10ac50f22e378dcbd300a
Dejah9 <khadejahhylton@yahoo.com>
2017-11-15 17:48:30 -0500

    edited exercise 4

--- world.js
+++ world.js
@@ -1,23 +1,21 @@
 /*global $*/
 window.onload = function(){
     let box = document.getElementById("check");
-    if(box.checked){
-        $("#lookup").click(function(e){
-            e.preventDefault();
-            $.ajax({
-                url:"world.php?all=true"
-            }).done(function(response) {
-                document.getElementById("result").innerHTML = response;
-            });   
-        });
-    }else{
-        $("#lookup").click(function(e){
-            e.preventDefault();
-            $.ajax({
-                url:"world.php?country=" + $('#country').val()
-            }).done(function(response) {
-                document.getElementById("result").innerHTML = response;
-            });
-        });
-    }
+   
+    $("#lookup").click(function(e){
+        e.preventDefault();
+        let url = "world.php?";
+        if($("#check").is(":checked")){
+            url+="all=true";
+        }
+        else{
+            url += "country="+ $('#country').val();
+        }
+
+        $.ajax({
+            url:url
+        }).done(function(response) {
+            document.getElementById("result").innerHTML = response;
+        });   
+    });
 };
